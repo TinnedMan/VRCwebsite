a43a37901a9cbd5b9c9bcf37173c23b7d6dce744
Joel tinned <Joelam95@outlook.com>
2020-01-29 12:28:58 +0800

    added comment for pablo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
+console.log("pablo you need to change this you egg thx")
+
 while (true) {
     console.log("im a memory leak!! hahaha!! by pablo")
     await new Promise(r => setTimeout(r, 2000));
 }
+
